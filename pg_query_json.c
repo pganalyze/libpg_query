cfd1a38ba06de3b10b912e069841c9face6cb8bb
Zac McCormick <zac.mccormick@gmail.com>
2016-05-17 15:15:07 -0400

    Escape JSON strings so all characters can be represented.
    This new function was taken directly from the postgres source.
    
    Sample query to reproduce problem:
    
    SELECT parse_ident(E'\"c\".X XXXX\002XXXXXX')

--- pg_query_json.c
+++ pg_query_json.c
@@ -107,32 +107,52 @@ removeTrailingDelimiter(StringInfo str)
 }
 
 static void
-_outToken(StringInfo str, const char *s)
+_outToken(StringInfo buf, const char *str)
 {
-	if (s == NULL)
+	if (str == NULL)
 	{
-		appendStringInfoString(str, "null");
+		appendStringInfoString(buf, "null");
 		return;
 	}
 
-	appendStringInfoChar(str, '"');
-	while (*s)
+	// copied directly from https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/json.c#L2428
+	const char *p;
+
+	appendStringInfoCharMacro(buf, '"');
+	for (p = str; *p; p++)
 	{
-		/* These chars must be backslashed anywhere in the string */
-		if (*s == '\n')
-			appendStringInfoString(str, "\\n");
-		else if (*s == '\r')
-			appendStringInfoString(str, "\\r");
-		else if (*s == '\t')
-			appendStringInfoString(str, "\\t");
-		else if (*s == '\\' || *s == '"') {
-			appendStringInfoChar(str, '\\');
-			appendStringInfoChar(str, *s);
-		} else
-			appendStringInfoChar(str, *s);
-		s++;
+		switch (*p)
+		{
+			case '\b':
+				appendStringInfoString(buf, "\\b");
+				break;
+			case '\f':
+				appendStringInfoString(buf, "\\f");
+				break;
+			case '\n':
+				appendStringInfoString(buf, "\\n");
+				break;
+			case '\r':
+				appendStringInfoString(buf, "\\r");
+				break;
+			case '\t':
+				appendStringInfoString(buf, "\\t");
+				break;
+			case '"':
+				appendStringInfoString(buf, "\\\"");
+				break;
+			case '\\':
+				appendStringInfoString(buf, "\\\\");
+				break;
+			default:
+				if ((unsigned char) *p < ' ')
+					appendStringInfo(buf, "\\u%04x", (int) *p);
+				else
+					appendStringInfoCharMacro(buf, *p);
+				break;
+		}
 	}
-	appendStringInfoChar(str, '"');
+	appendStringInfoCharMacro(buf, '"');
 }
 
 static void
