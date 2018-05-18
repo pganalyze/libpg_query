d40e0b52fef1909f41db8f793d7636f5d6f9035e
Lukas Fittl <lukas@fittl.com>
2018-05-18 12:54:53 +0200

    pg_query_parse_plpgsql: Explicitly ignore asprintf result to avoid warning
    See https://github.com/lfittl/pg_query_go/pull/14

--- src/pg_query_parse_plpgsql.c
+++ src/pg_query_parse_plpgsql.c
@@ -430,11 +430,12 @@ PgQueryPlpgsqlParseResult pg_query_parse_plpgsql(const char* input)
 		if (func_and_error.func != NULL) {
 			char *func_json;
 			char *new_out;
+			int ignored;
 
 			func_json = plpgsqlToJSON(func_and_error.func);
 			plpgsql_free_function_memory(func_and_error.func);
 
-			asprintf(&new_out, "%s%s,\n", result.plpgsql_funcs, func_json);
+			ignored = asprintf(&new_out, "%s%s,\n", result.plpgsql_funcs, func_json);
 			free(result.plpgsql_funcs);
 			result.plpgsql_funcs = new_out;
 
