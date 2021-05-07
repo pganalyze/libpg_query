3a923037d06846f202079f806a5257c34269b685
davkor <david@adalogics.com>
2021-05-07 18:08:18 +0100

    test: fuzz: fix indentation.
    Signed-off-by: davkor <david@adalogics.com>

--- test/fuzz/fuzz_parser.c
+++ test/fuzz/fuzz_parser.c
@@ -8,7 +8,7 @@
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 	char *new_str = (char *)malloc(size+1);
 	if (new_str == NULL){
-			return 0;
+        return 0;
 	}
 	memcpy(new_str, data, size);
 	new_str[size] = '\0';
