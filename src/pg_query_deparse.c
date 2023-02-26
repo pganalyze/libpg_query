b66f9eff1fe64674cac65b858412a1645b5549c3
Lukas Fittl <lukas@fittl.com>
2023-02-25 23:42:16 -0800

    Deparser: Ensure index names are quoted as identifiers
    Fixes https://github.com/pganalyze/pg_query_go/issues/77

--- src/pg_query_deparse.c
+++ src/pg_query_deparse.c
@@ -7938,7 +7938,7 @@ static void deparseIndexStmt(StringInfo str, IndexStmt *index_stmt)
 
 	if (index_stmt->idxname != NULL)
 	{
-		appendStringInfoString(str, index_stmt->idxname);
+		appendStringInfoString(str, quote_identifier(index_stmt->idxname));
 		appendStringInfoChar(str, ' ');
 	}
 
