8e91de2e2e0740c570b236e1045b74dfd382ba4b
Lukas Fittl <lukas@fittl.com>
2024-01-01 11:24:28 -0800

    Windows support: Use pg_config_os.h to include port/win32.h if needed
    We previously had the hardcoded macOS ("darwin") contents for this file,
    which is unnecessary and incorrect. Instead keep this file empty in all
    cases except on Windows, where we include port/win32.h (just like Postgres
    itself does), since it is actually necessary for our use case.
    
    Different from Postgres itself we undef PGDLLIMPORT and PGDLLEXPORT, since
    they are not needed for pg_query, and PGDLLIMPORT conflicts with the
    thread local attribute.
    
    Additionally, we workaround a Windows-specific problem with sigsetjmp
    defines when using clang whilst __MINGW64__ is defined (which can happen
    on MSYS2-based environments).

--- src/postgres/include/pg_config_os.h
+++ src/postgres/include/pg_config_os.h
@@ -1,8 +1,8 @@
-/* src/include/port/darwin.h */
-
-#define __darwin__	1
-
-#if HAVE_DECL_F_FULLFSYNC		/* not present before macOS 10.3 */
-#define HAVE_FSYNC_WRITETHROUGH
-
+#if defined(_WIN32) || defined(_WIN64)
+#include "port/win32.h"
+#undef PGDLLIMPORT
+#undef PGDLLEXPORT
+#ifdef __clang__
+#undef __MINGW64__
+#endif /* __clang__ */
 #endif
