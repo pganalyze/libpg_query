bf05741350ee4a5e8cb6dcbe57680ce8d5ea8fd2
Dan Lynch <pyramation@gmail.com>
2025-06-30 01:48:34 -0700

    Allow all integer values

--- src/pg_query_outfuncs_json.c
+++ src/pg_query_outfuncs_json.c
@@ -197,8 +197,7 @@ _outOidList(StringInfo out, const List *node)
 static void
 _outInteger(StringInfo out, const Integer *node)
 {
-	if (node->ival > 0)
-		appendStringInfo(out, "\"ival\":%d", node->ival);
+	appendStringInfo(out, "\"ival\":%d", node->ival);
 }
 
 static void
