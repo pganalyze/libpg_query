6a91c2e6cba0bd0eca7c3ca27eb447e273f787f7
Ellen Marie Dash <me@duckie.co>
2026-05-28 18:31:07 -0400

    Add normalize tests for NOTIFY statements.

--- test/normalize_tests.c
+++ test/normalize_tests.c
@@ -42,6 +42,14 @@ const char* tests[] = {
   "MERGE into measurement m USING new_measurement nm ON (m.city_id = nm.city_id and m.logdate=nm.logdate) WHEN MATCHED AND nm.peaktemp IS NULL THEN DELETE WHEN MATCHED THEN UPDATE SET peaktemp = greatest(m.peaktemp, nm.peaktemp), unitsales = m.unitsales + coalesce(nm.unitsales, $1) WHEN NOT MATCHED THEN INSERT (city_id, logdate, peaktemp, unitsales) VALUES (city_id, logdate, peaktemp, unitsales)",
   "DO language E'plpgsql' 'BEGIN PERFORM 1; END'",
   "DO language $1 $2",
+  "NOTIFY channel, 'a'",
+  "NOTIFY channel, $1",
+  "NOTIFY channel, 'b'",
+  "NOTIFY channel, $1",
+  "NOTIFY channel, 'some text to normalize'",
+  "NOTIFY channel, $1",
+  "NOTIFY channel",
+  "NOTIFY channel",
   // These below are as expected, though questionable if upstream shouldn't be
   // fixed as this could bloat pg_stat_statements
   "DECLARE cursor_b CURSOR FOR SELECT * FROM x WHERE id = 123",
